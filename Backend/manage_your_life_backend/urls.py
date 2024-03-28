0ecc93fe8f0342763a6ad53d2cb5ce40186e2b1c
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2024-03-27 21:34:44 -0300

    I create myapp => login , where it should be the login

--- Backend/manage_your_life_backend/urls.py
+++ Backend/manage_your_life_backend/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from login.views import login
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', login)
 ]
